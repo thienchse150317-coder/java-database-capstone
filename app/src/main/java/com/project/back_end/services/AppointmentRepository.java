3d95e3045625f05bda9591b33c03d1baf3a0dc6f
thienchse150317 <thienchse150317@fpt.edu.vn>
2025-09-22 02:07:44 -0500

    Create AppointmentRepository.java

---
+++ app/src/main/java/com/project/back_end/services/AppointmentRepository.java
@@ -0,0 +1,17 @@
+package com.project.back_end.repositories;
+
+import com.project.back_end.models.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorIdAndAppointmentTimeBetween(
+            Long doctorId,
+            LocalDateTime startOfDay,
+            LocalDateTime endOfDay
+    );
+}

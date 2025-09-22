fae189fa52b321eae2db1cde135091f15be1f987
thienchse150317 <thienchse150317@fpt.edu.vn>
2025-09-22 02:13:01 -0500

    Update PatientRepository.java

--- app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -1,1 +1,17 @@
-https://github.com/thienchse150317/java-database-capstone
+package com.project.back_end.repo;
+
+import com.project.back_end.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    
+    // Find patient by email
+    Optional<Patient> findByEmail(String email);
+
+    // Find patient by email or phone number
+    Optional<Patient> findByEmailOrPhone(String email, String phone);
+}

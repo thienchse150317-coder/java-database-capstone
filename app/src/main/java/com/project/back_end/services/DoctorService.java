2e08957cdc25d05111918e59e8defae58372dab7
thienchse150317 <thienchse150317@fpt.edu.vn>
2025-09-22 02:17:46 -0500

    Update DoctorService.java

--- app/src/main/java/com/project/back_end/services/DoctorService.java
+++ app/src/main/java/com/project/back_end/services/DoctorService.java
@@ -1,1 +1,36 @@
-https://github.com/thienchse150317-coder/java-database-capstone/blob/main/app/src/main/java/com/project/back_end/services/DoctorService.java
+package com.project.back_end.services;
+
+import com.project.back_end.model.Doctor;
+import com.project.back_end.repo.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    // Get all doctors
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    // Get doctor by ID
+    public Optional<Doctor> getDoctorById(Long id) {
+        return doctorRepository.findById(id);
+    }
+
+    // Save or update doctor
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    // Delete doctor by ID
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
+}
